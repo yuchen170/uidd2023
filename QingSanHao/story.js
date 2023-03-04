05161851d744563ce3336f7d3aefa027462927f6
Yu Chen Yen <yuchenyen170@gmail.com>
2023-03-05 00:48:33 +0800

    animation

--- QingSanHao/story.js
+++ QingSanHao/story.js
@@ -1,8 +1,49 @@
+
 gsap.config({
     nullTargetWarn: false
 });
-let tl = gsap.timeline({})
 
+document.addEventListener("DOMContentLoaded",function(){
+    
+    var loading=new TimelineMax();
+    loading.fromTo("#main",1.5,{
+        autoAlpha: 0,
+        y:0
+    },{
+        autoAlpha: 1,
+        y:20
+    })
+    .fromTo("#first",1.5,{
+        autoAlpha: 0,
+        y:0
+    },{
+        autoAlpha: 1,
+        y:20
+    })
+    .fromTo("#second",1.5,{
+        autoAlpha: 0,
+        y:0
+    },{
+        autoAlpha: 1,
+        y:20
+    })
+    .fromTo("#third",1.5,{
+        autoAlpha: 0,
+        y:0
+    },{
+        autoAlpha: 1,
+        y:20
+    })
+    .fromTo("#main",1.5,{
+        autoAlpha: 1,
+        y:20
+    },{
+        autoAlpha: 0,
+        y:40
+    })
+});
+
+let tl = gsap.timeline({})
 .to(".first", { y: 100 })
 .to(".second", { y: 100 })
 .to(".third", { y: 100 });
