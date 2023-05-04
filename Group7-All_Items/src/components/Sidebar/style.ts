3edc75cdd023b8e8506ee6d223cb672aeb4d41b5
yuchen170 <yuchenyen170@gmail.com>
2023-05-04 14:30:11 +0800

    rm

--- Group7-All_Items/src/components/Sidebar/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Arrow = styled.div`
-  max-width: 10px;
-  margin-top: 5px;
-  display: flex;
-  justify-content: flex-end;
-  flex-grow: 1;
-`;
--- Group7-All_Items/src/components/atoms/Section/style.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const Section = styled.section`
-    padding-bottom: 50px;
-`;
